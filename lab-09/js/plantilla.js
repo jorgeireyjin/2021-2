eefe34af2a22bc7c9efd475ecb5bdee7b2c36df8
Jin <jorge.irey.jin@outlook.com>
2021-10-01 11:44:05 -0500

    Clase 01OCT

---
+++ lab-09/js/plantilla.js
@@ -0,0 +1,34 @@
+/*
+Los URLS
+*/
+
+
+
+/*
+Crear los nodos necesarios y retornar una fila HTML
+*/
+
+
+/*
+Obtener los datos
+*/
+
+
+/*
+ Agregar registro
+ */
+
+
+/*
+ Eliminar nodo solo en el DOM
+ */
+
+
+/*
+Refrescar pantalla 
+*/
+
+
+/***************************************/
+/* PROGRAMA PRINCIPAL */
+/***************************************/
