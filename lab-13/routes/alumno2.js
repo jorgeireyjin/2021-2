4c9a39ff458c28273a02ee1bb6bdeb6f3d2f348e
Jin <jorge.irey.jin@outlook.com>
2021-10-29 20:23:02 -0500

    Clase 29OCT

--- lab-13/routes/alumno2.js
+++ lab-13/routes/alumno2.js
@@ -33,5 +33,28 @@ ruta.route('/ejemplo4')
             res.json(superh);
     }) 
 
+ruta.route('/ejemplo5')
+    .get( (req,res,next) => {
+        // Vamos a suponer que hay un procesamiento  y btemos unos datos de algun lado
+        var superh = [
+            { name : "Tony Stark", organizacion: "Stark Industries", power: 5},
+            { name : "Bruno Diaz", organizacion: "Batman Inc", power: 2},
+            { name : "Bob Esponja", organizacion: "Nicklodeon", power: 0}
+        ]
+
+        var frase = "Al infinito y mas alla ...";
+
+        if ( !req.session.contador ) {
+            req.session.contador = 1;
+        } else {
+            req.session.contador++;
+        }
+
+        res.locals.contadorVista = req.session.contador;
+
+        res.render("welcome4", { p1: superh, p2: frase, layout : '../layouts/plantilla3' } );
+
+    }) 
+
 // NO OLVIDAR EXPORTAR
 module.exports = ruta;
