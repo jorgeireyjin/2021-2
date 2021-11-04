b5449b665123fa67f3f84126b81c0215136d873d
Jin <jorge.irey.jin@outlook.com>
2021-11-03 21:39:36 -0500

    Delete y update

--- lab-15/routes/usuarioRutas.js
+++ lab-15/routes/usuarioRutas.js
@@ -5,7 +5,11 @@ const models = require("../models")
 
 // Modulos de acceso a BD
 const query = require("../persistencia/selectall")
+const query1 = require("../persistencia/selectfiltro")
 const inserta = require("../persistencia/insert")
+const elimina = require("../persistencia/delete");
+const actualiza = require("../persistencia/update")
+
 
 const usuario = models.Usuario;
 
@@ -64,6 +68,69 @@ rutas.route("/agregar")
 
     })
 
+// La ruta para eliminar un registro
+rutas.route("/eliminar")
+    .post( async (req,res,next) => {
+        await elimina(
+            req.body.id
+        )
+        .then( async () =>{
+            await query()
+            .then( (listado) => {
+                res.render('principal',{ p1: listado, layout: "../layouts/plantilla2"})
+            } )
+            .catch( (error) => {
+                console.log("Ocurrio un error en el query", error)
+            })                
+        })
+        .catch( (error) => {
+            console.log("Ocurrio un error en el delete", error)
+        })
+
+    })
+
+    
+// La ruta para editar
+rutas.route("/editar")
+    .post( async (req,res,next) => {
+        // Debo ir a la BD a obtener la informacion
+        // y mostrarla en un form
+        await query1(
+            req.body.id
+        )
+        .then( (reg) =>{
+            res.render("editar", {p0: reg , layout: "../layouts/plantilla1"})
+        })
+        .catch( (error) =>{
+            console.log("Error en query1 ", error)
+        })
+
+    })
+
+// La ruta para actualizar
+rutas.route("/actualizar")
+    .post( async (req,res,next) => {
+        // Debo actualizar la data en la BD
+        // Mostrar la vista principal
+        
+        await actualiza(
+            req.body.codigo,
+            req.body.nombre,
+            req.body.edad
+            )
+            .then( async () =>{
+                await query()
+                .then( (listado) => {
+                    res.render('principal',{ p1: listado, layout: "../layouts/plantilla2"})
+                } )
+                .catch( (error) => {
+                    console.log("Ocurrio un error en el query", error)
+                })         
+            })
+            .catch( (error) => {
+                console.log("error en actualiza ...")
+            })
+    })
 
 // No olvidar
 module.exports = rutas
